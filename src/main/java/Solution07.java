61c1c5ab770eb2cec795901541209733c094f2bc
Patrick <patrickbrookshire@Knights.ucf.edu>
2021-09-19 18:49:34 -0400

    Commit

---
+++ src/main/java/Solution07.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Solution07 {
+
+    public static void main(String[] agrs) {
+
+        Scanner input = new Scanner(System.in);
+
+        final double f, m, g = 0.09290304;
+        int x, y;
+
+        System.out.println("What is the length of the room in feet?");
+        x = input.nextInt();
+
+        System.out.println("What is the length of the room in feet?");
+        y = input.nextInt();
+
+        System.out.println("You entered dimensions of " + x + " feet by " + y + " feet.");
+
+        f = x * y;
+        m = f * g;
+
+        System.out.println("The area is \r\n" + f + " square feet \r\n" + m + " sqaure meters");
+
+
+
+
+    }
+
+}
